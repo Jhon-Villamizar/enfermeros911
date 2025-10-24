06ac8a235aa938d646ac4862c06ab7eb082c178d
Jhon Villamizar <jhonvio@hotmail.com>
2025-10-23 19:27:25 -0500

    fisrt commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+   server: {
+    host: true,
+    port: 3001,
+    watch: {
+      usePolling: true,
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: ['index.html', '404.html'],
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    ssr: false,
+  },
+  base: '/',
+})
